001282f73040c780c5ba6dcf632fa757f64209cd
andreicode <andreipopa.1992@gmail.com>
2017-06-07 00:42:24 +0300

    token-api-update

--- ANDREI/API/app.js
+++ ANDREI/API/app.js
@@ -53,7 +53,7 @@ app.post('/login/facebook', function (req, res) {
 
     httpsGetRequest(options).then(function (response) {
 
-        console.log(response);
+        // console.log(response);
         const accessToken = response.access_token;
         const options = {
             hostname: 'graph.facebook.com',
@@ -65,101 +65,25 @@ app.post('/login/facebook', function (req, res) {
 
         httpsGetRequest(options).then(function (response) {
 
-            console.log('2', response);
+            // console.log('2', response);
+
+            const payload = {
+
+                facebookId: response.id,
+                name: response.name,
+                email: response.email,
+            };
+
+            const token = jwt.sign(payload, 'appsecret');
+
+            // console.log(token);
+
+            res.status(200).json({token: token});
 
         });
 
     });
 
-    // console.log('asdadaw123')
-
-    // const reqAccessToken = https.request(options, (res) => {
-
-    //     var data = [];
-
-    //     res.on('data', (d) => {
-
-    //         data.push(d);
-
-    //         console.log('132213');
-
-    //     });
-
-    //     res.on('end', function () {
-
-
-    //         const buffer = Buffer.concat(data);
-    //         const response = JSON.parse(buffer.toString());
-
-    //         const accessToken = response.access_token;
-
-    //         const options2 = {
-    //             hostname: 'graph.facebook.com',
-    //             port: 443,
-    //             path: '/v2.9/me?access_token=' + access_token + '&fields=id,name,email,picture',
-    //             method: 'GET',
-    //             json: true
-
-    //         }
-
-    //         console.log('asdsa');
-
-    //         const requestUserInfo = https.request(options2, (res) => {
-
-    //             console.log('asda');
-
-    //             var data = [];
-
-    //             res2.on('data', (d) => {
-
-    //                 data.push(d);
-
-    //             });
-
-    //             res.on('end', function () {
-
-
-    //                 const buffer = Buffer.concat(data);
-    //                 const response = JSON.parse(buffer.toString());
-
-    //                 console.log('a', response);
-
-
-    //             });
-
-    //             requestUserInfo.on('error', (e) => {
-
-    //                 console.log(e);
-
-    //             });
-
-    //             requestUserInfo.end();
-
-    //         });
-
-    //     });
-
-    //     reqAccessToken.on('error', (e) => {
-
-    //         console.log('122312321', e);
-
-    //     });
-
-    //     reqAccessToken.end();
-
-
 });
-
-
-
-app.get('/auth/facebook/callback',
-    function (req, res) {
-        console.log('asdsadsada');
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
-
-
-
 
 app.listen(3000);
