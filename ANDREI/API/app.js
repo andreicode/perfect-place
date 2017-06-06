fe80527a033abdc3b3063c1bdbfebc936bdd8573
andreicode <andreipopa.1992@gmail.com>
2017-06-07 00:13:20 +0300

    added fb login

--- ANDREI/API/app.js
+++ ANDREI/API/app.js
@@ -15,6 +15,24 @@ app.get('/hello', function (req, res) {
 
 })
 
+function httpsGetRequest(options) {
+
+    return new Promise((resolve, reject) => {
+
+        https.request(options, (res) => {
+            const data = [];
+
+            res.on('data', (chunk) => { data.push(chunk) });
+            res.on('end', () => { resolve(JSON.parse(Buffer.concat(data))) });
+
+        })
+            .on('error', (err) => { console.log('err', err) })
+            .end();
+
+    });
+
+};
+
 app.post('/login/facebook', function (req, res) {
 
     const clientId = req.body.clientId;
@@ -32,78 +50,103 @@ app.post('/login/facebook', function (req, res) {
         json: true
     };
 
-    const reqAccessToken = https.request(options, (res) => {
 
-        var data = [];
+    httpsGetRequest(options).then(function (response) {
 
-        res.on('data', (d) => {
+        console.log(response);
+        const accessToken = response.access_token;
+        const options = {
+            hostname: 'graph.facebook.com',
+            port: 443,
+            path: '/v2.9/me?access_token=' + accessToken + '&fields=id,name,email,picture',
+            method: 'GET',
+            json: true
+        };
 
-            data.push(d);
+        httpsGetRequest(options).then(function (response) {
 
-        });
-
-        res.on('end', function () {
-
-
-            const buffer = Buffer.concat(data);
-            const response = JSON.parse(buffer.toString());
-
-            const accessToken = response.access_token;
-
-            const options2 = {
-                hostname: 'graph.facebook.com',
-                port: 443,
-                path: '/v2.9/me?access_token=' + access_token + '&fields=id,name,email,picture',
-                method: 'GET',
-                json: true
-
-            }
-
-            const requestUserInfo = https.request(options2, (res2) => {
-
-                console.log('asda');
-
-                var data = [];
-
-                res2.on('data', (d) => {
-
-                    data.push(d);
-
-                });
-
-
-                res2.on('end', function () {
-
-
-                    const buffer = Buffer.concat(data);
-                    const response = JSON.parse(buffer.toString());
-
-                    console.log('a', response);
-
-
-                });
-
-                requestUserInfo.on('error', (e) => {
-
-                    console.log(e);
-
-                });
-
-                requestUserInfo.end();
-
-            });
+            console.log('2', response);
 
         });
-
-        reqAccessToken.on('error', (e) => {
-
-            console.log(e);
-
-        });
-
-        reqAccessToken.end();
 
     });
+
+    // console.log('asdadaw123')
+
+    // const reqAccessToken = https.request(options, (res) => {
+
+    //     var data = [];
+
+    //     res.on('data', (d) => {
+
+    //         data.push(d);
+
+    //         console.log('132213');
+
+    //     });
+
+    //     res.on('end', function () {
+
+
+    //         const buffer = Buffer.concat(data);
+    //         const response = JSON.parse(buffer.toString());
+
+    //         const accessToken = response.access_token;
+
+    //         const options2 = {
+    //             hostname: 'graph.facebook.com',
+    //             port: 443,
+    //             path: '/v2.9/me?access_token=' + access_token + '&fields=id,name,email,picture',
+    //             method: 'GET',
+    //             json: true
+
+    //         }
+
+    //         console.log('asdsa');
+
+    //         const requestUserInfo = https.request(options2, (res) => {
+
+    //             console.log('asda');
+
+    //             var data = [];
+
+    //             res2.on('data', (d) => {
+
+    //                 data.push(d);
+
+    //             });
+
+    //             res.on('end', function () {
+
+
+    //                 const buffer = Buffer.concat(data);
+    //                 const response = JSON.parse(buffer.toString());
+
+    //                 console.log('a', response);
+
+
+    //             });
+
+    //             requestUserInfo.on('error', (e) => {
+
+    //                 console.log(e);
+
+    //             });
+
+    //             requestUserInfo.end();
+
+    //         });
+
+    //     });
+
+    //     reqAccessToken.on('error', (e) => {
+
+    //         console.log('122312321', e);
+
+    //     });
+
+    //     reqAccessToken.end();
+
 
 });
 
