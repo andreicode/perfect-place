0e95ca23f98697acaafb64555d901d0196962bbf
karinamatrana <karina.matrana@gmail.com>
2017-06-08 23:32:24 +0300

    Stash

---
+++ API/seeders/20170608201242-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const faker = require('faker');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert('users', [{
+      name: 'John Doe',
+      isBetaMember: false
+    }], {});
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
