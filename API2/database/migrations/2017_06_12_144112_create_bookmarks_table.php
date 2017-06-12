bf36d58bdd81c1d7625bd09f7405f435c40e277d
karinamatrana <karina.matrana@gmail.com>
2017-06-12 17:48:35 +0300

    Am adaugat migrare bookmarks

---
+++ API2/database/migrations/2017_06_12_144112_create_bookmarks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->integer('user_id')->references('id')->on('users');
+            $table->integer('listing_id')->references('id')->on('listings');
+            $table->primary(['user_id', 'listing_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
